85f8540be4fcd796dd1336e410efb2f8d966757b
Kudz <kudzayibamhare@gmail.com>
2023-09-27 15:38:08 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,22 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return List.of(new Student("Luigi",
+                "greenguy@nin.com",
+                LocalDate.of(1985, Month.AUGUST, 8),
+                38));
+    }
+}
